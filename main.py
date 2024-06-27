a843645be65c0ddddf4c9b5f625c33eb48355c65
isp05 <pavelsedunov2005official@mail.ru>
2024-06-27 18:44:19 +0300

    last commit

--- main.py
+++ main.py
@@ -95,3 +95,19 @@ def delete_employee(db: Session, employee_id: int):
     db.delete(db_employee)
     db.commit()
     return {'detail': 'Employee deleted'}
+
+@app.post('/employees/', response_model=Employee)
+def create_employee(employee: EmployeeCreate, db: Session = Depends(get_db)):
+    return crud.create_employee(db, employee)
+
+@app.get('/employees/{employee_id}', response_model=Employee)
+def read_employee(employee_id: int, db: Session = Depends(get_db)):
+    return crud.read_employee(db, employee_id)
+
+@app.put('/employees/{employee_id}', response_model=Employee)
+def update_employee(employee_id: int, employee_update: EmployeeUpdate, db: Session = Depends(get_db)):
+    return crud.update_employee(db, employee_id, employee_update)
+
+@app.delete('/employees/{employee_id}')
+def delete_employee(employee_id: int, db: Session = Depends(get_db)):
+    return crud.delete_employee(db, employee_id)
