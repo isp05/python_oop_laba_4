7f0915a05f7a49216af3f70bbc1d21250c0ba1ec
isp05 <pavelsedunov2005official@mail.ru>
2024-06-27 18:30:34 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import datetime
+import sqlalchemy
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.exc import IntegrityError
+
+app = FastAPI()
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://isp_p_Sedunov:12345@77.91.86.135/isp_p_Sedunov"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+class Employee(Base):
+    __tablename__ = 'employees'
+
+    id = Column(Integer, primary_key=True, index=True)
+    department_code = Column(Integer)
+    full_name = Column(String)
+    position = Column(String)
+    salary = Column(Float)
+    bonus = Column(Float)
+    month = Column(String)
+
+
+class Contract(Base):
+    __tablename__ = 'contracts'
+
+    id = Column(Integer, primary_key=True, index=True)
+    number = Column(String, unique=True)
+    organization_name = Column(String)
+    date_signed = Column(Date)
+    employee_id = Column(Integer, ForeignKey('employees.id'))
+
+
+class Organization(Base):
+    __tablename__ = 'organizations'
+
+    id = Column(Integer, primary_key=True, index=True)
+    contract_id = Column(Integer, ForeignKey('contracts.id'))
+    country_code = Column(String)
+    city = Column(String)
+    address = Column(String)
+    phone = Column(String)
+    email = Column(String)
+    website = Column(String)
+
+class Delivery(Base):
+    __tablename__ = 'deliveries'
+
+    id = Column(Integer, primary_key=True, index=True)
+    contract_id = Column(Integer, ForeignKey('contracts.id'))
+    equipment_type = Column(String)
+    user_comment = Column(String)
+    employee_id = Column(Integer, ForeignKey('employees.id'))
+
+Base.metadata.create_all(bind=engine)
